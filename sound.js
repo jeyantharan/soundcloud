1d68eadbded545eaf65f8368bebfdf9ef3dc6197
jeyantharan <87352558+jeyantharan@users.noreply.github.com>
2021-08-09 15:54:52 +0530

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,18 +1,17 @@
 SC.initialize({
-  client_id: '340f063c670272fac27cfa67bffcafc4'
-});
-$(document).ready(function() {
- SC.stream('/tracks/285384503',function(sound){
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
 
-   $('#start').click(function(e) {
-              e.preventDefault();
-              sound.start();
-            });
-    $('#stop').click(function(e) {
-              e.preventDefault();
-              sound.stop();
-             });
- });
+  $(document).ready(function() {
+    SC.stream('/tracks/1006086883',function(sound){
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
 
-
- });
+        $('#stop').click(function(e) {
+        e.preventDefault();
+        sound.stop();
+        });
+    });
+  });
